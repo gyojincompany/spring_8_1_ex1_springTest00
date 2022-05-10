eae52b35255d249e61c6ce232dc6666a2800f532
JBEDU000 <JBEDU000@JBEDU00>
2022-05-10 13:18:51 +0900

    외부파일로 스프링 빈 초기값 설정(Environment 객체사용)

---
+++ src/main/java/com/gyojincompany/spring_8_1/AdminConnection.java
@@ -0,0 +1,64 @@
+package com.gyojincompany.spring_8_1;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+public class AdminConnection implements InitializingBean, DisposableBean, EnvironmentAware {
+	
+	private String adminId;
+	private String adminPw;
+	private Environment env;
+	
+	@Override
+	public void setEnvironment(Environment env) {
+		// TODO Auto-generated method stub
+		System.out.println("setEnvironment 메서드 호출!!");
+		setEnv(env);
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 메서드 호출!!");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {//빈이 초기화될때 자동 호출
+		// TODO Auto-generated method stub
+		System.out.println("afterPropertiesSet 메서드 호출!!");
+		setAdminId(env.getProperty("admin.Id"));
+		setAdminPw(env.getProperty("admin.Pw"));
+
+	}
+
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminPw() {
+		return adminPw;
+	}
+
+	public void setAdminPw(String adminPw) {
+		this.adminPw = adminPw;
+	}
+
+	public Environment getEnv() {
+		return env;
+	}
+
+	public void setEnv(Environment env) {
+		this.env = env;
+	}
+
+	
+	
+	
+
+}
